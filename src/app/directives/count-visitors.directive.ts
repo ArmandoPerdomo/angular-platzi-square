a18933d47a51d0a43ce04370b82bcb197d5140a4
Armando Perdomo <armando.perdomo@arkiteck.biz>
2018-07-26 22:16:09 -0400

    ...

--- src/app/directives/count-visitors.directive.ts
+++ src/app/directives/count-visitors.directive.ts
@@ -1,12 +1,17 @@
 import { Directive, HostListener, Input } from "../../../node_modules/@angular/core";
+import { Router } from "../../../node_modules/@angular/router";
 
 @Directive({
     selector: 'li[count-visitors]'
 })
 export class CountVisitors{
+
+    constructor(private _router: Router){}
+
     @Input('count-visitors') place :any;
     @HostListener('click', ['$event.target']) countVisits() {
         this.place.visits++;
+        this._router.navigate(['/details'],{ queryParams: { id: this.place.id } });
         console.log('Numero de visitas', this.place.visits);
     }
 }
--- src/app/modules/details/details.component.ts
+++ src/app/modules/details/details.component.ts
@@ -8,11 +8,25 @@ import { ActivatedRoute } from '../../../../node_modules/@angular/router';
 })
 export class DetailsComponent implements OnInit {
 
+  places: any = [
+    {id: 1, plan: 'free', closeness: 1, distance: 0.50, name: 'Barsoke' , visits: 0},
+    {id: 2, plan: 'premiun', closeness: 2, distance: 4.50, name: 'Arkiteck' , visits: 0},
+    {id: 3, plan: 'free', closeness: 1, distance: 1, name: 'Panadería la Alianza' , visits: 0},
+    {id: 4, plan: 'premiun', closeness: 3, distance: 8.50, name: 'Los Girasoles' , visits: 0},
+    {id: 5, plan: 'premiun', closeness: 3, distance: 8, name: 'La Hacienda' , visits: 0},
+    {id: 6, plan: 'free', closeness: 1, distance: 0.50, name: 'La Venus' , visits: 0},
+    {id: 7, plan: 'free', closeness: 3, distance: 12, name: 'Maro\'s Café' , visits: 0},
+    {id: 8, plan: 'free', closeness: 2, distance: 6.50, name: 'El Paso' , visits: 0}
+  ];
+
+  id = null;
+  place = {};
   constructor(private route: ActivatedRoute) {
-    console.log(this.route.snapshot.params['id']);
+    this.id = this.route.snapshot.queryParams['id'];
   }
 
   ngOnInit() {
+
   }
 
 }
